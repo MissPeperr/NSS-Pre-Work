4a5361fff9b8e028927c7bdd46f13e3c0a92f41a
Madi Peper <madisonpeper@gmail.com>
2019-06-06 11:48:20 -0500

    added objects to live-code

---
+++ live-code-examples/objects.js
@@ -0,0 +1,30 @@
+///// OBJECTS /////
+/*
+    Arrays are great if we want to store data in order, but they have one big limitation: they can only keep track of our data by index. What if we want to label and organize our data in more detail? For example, let's say we want to store information about the students in a class instead of just their grades. Objects to the rescue!
+*/
+
+// Let's make an object with 3 "key-value pairs": firstName, lastName, and age
+
+
+
+// If we want to get access to certain keys inside of an object, we can use object notation
+// Let's log to the console the firstName and age of our object:
+
+
+
+
+// Just like arrays, objects can hold any other data type- including arrays and objects:
+// Let's build a few objects that represent some students:
+
+
+
+// Now let's dig into our objects using dot notation, and log to the console various different properties:
+
+
+
+// We can also have arrays of objects! Aaah!
+// Let's create an array, that has all of our objects that we created inside:
+
+
+
+// Now let's try accessing certain properties inside of our objects, that are inside of the array:
